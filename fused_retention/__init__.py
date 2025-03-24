implemented POP fwd+bwd+tests; improved numerical stability of chunk retention, possibly at the expense of speed, but it's currently impossible to benchmark times due to a tilelang bug;

fused_retention/__init__.py
@@ -1,10 +1,17 @@
 import torch
 import tilelang
-from tilelang import cached
 from einops import rearrange
 
 from .fused_chunk_fwd import fused_chunk_retention_fwd
 from .fused_chunk_bwd import fused_retention_bwd_dk_dv_ds, fused_retention_bwd_dq
+
+
+kernel_cache = {}
+def _get_kernel(key, fn):
+    if key in kernel_cache:
+        return kernel_cache[key]
+    kernel_cache[key] = fn()
+    return kernel_cache[key]
 
 
 class FusedChunkRetention(torch.autograd.Function):
@@ -14,15 +21,19 @@ class FusedChunkRetention(torch.autograd.Function):
         batch_size, seq_len, num_heads, dim_qk = q.shape
         dim_v = v.shape[-1]
 
-        block_K, block_V, block_T = 64, 64, 64
+        block_K, block_V, block_T = 64, 64, 32  # for GPUs with large memory try BT=64
 
         assert len(head_decays) == num_heads
         chunk_decays = _get_decay_mask(head_decays, block_T)
 
-        mod = tilelang.cached(fused_chunk_retention_fwd, [5, 6], batch_size, num_heads, seq_len, dim_qk, dim_v, block_K, block_V, block_T)
-        o, s_new = mod(q, k, v, s, chunk_decays)
+        # key = ('fused_chunk_retention_fwd', batch_size, num_heads, seq_len, dim_qk, dim_v, block_K, block_V, block_T)
+        f = fused_chunk_retention_fwd(batch_size, num_heads, seq_len, dim_qk, dim_v, block_K, block_V, block_T, threads=64)
+        # fn = lambda: tilelang.compile(f, [5, 6], target='cuda')
+        # mod = _get_kernel(key, fn)
+        mod = tilelang.cached(f, [5, 6], target='cuda')
+        o, s_out = mod(q, k, v, s, chunk_decays)
         ctx.save_for_backward(q, k, v, s, chunk_decays)
-        return o.sum(0), s_new.sum(0)
+        return o.sum(0), s_out.sum(0)
 
     @staticmethod
     def backward(ctx, dO, dS_new):
@@ -30,11 +41,11 @@ class FusedChunkRetention(torch.autograd.Function):
 
         batch_size, seq_len, num_heads, dim_qk = q.shape
         dim_v = v.shape[-1]
-        block_K, block_V, block_T = 64, 64, 64
+        block_K, block_V, block_T = 64, 64, 32  # for GPUs with large memory try BT=64
 
-        mod = tilelang.cached(fused_retention_bwd_dk_dv_ds, [6, 7, 8], batch_size, num_heads, seq_len, dim_qk, dim_v, block_K, block_V, block_T)
+        mod = tilelang.compile(fused_retention_bwd_dk_dv_ds(batch_size, num_heads, seq_len, dim_qk, dim_v, block_K, block_V, block_T), [6, 7, 8], target='cuda')
         dK, dV, dS = mod(q, k, v, chunk_decays, dO, dS_new)
-        mod2 = tilelang.cached(fused_retention_bwd_dq, [5], batch_size, num_heads, seq_len, dim_qk, dim_v, block_K, block_V, block_T)
+        mod2 = tilelang.compile(fused_retention_bwd_dq(batch_size, num_heads, seq_len, dim_qk, dim_v, block_K, block_V, block_T, threads=64), [5], target='cuda')
         dQ = mod2(k, v, s, chunk_decays, dO)
 
         return dQ.sum(0), dK.sum(0), dV.sum(0), dS.sum(0), None

test_fused_retention.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from math import ceil
 
 import torch
+import tilelang
 from tilelang.profiler import do_bench
 from einops import rearrange
 from loguru import logger
@@ -14,9 +15,6 @@ detail_level = "detail"
 logger.level(detail_level, no=15, color="<yellow>")
 # logger.remove()
 # logger.add(sys.stderr, level="INFO")
-
-# from fla.ops.linear_attn import fused_chunk_linear_attn as chunk_linear_attn
-# from fla.ops.linear_attn import chunk_linear_attn
 
 from fused_retention import fused_chunk_retention
 from fused_retention.reference import ref_program, reference_grads
@@ -38,9 +36,9 @@ def get_mean_abs_err(x, y):
     return (x - y).flatten().abs().mean().item()
 
 
-def get_err_ratio(x, y):
-    err = (x - y).flatten().square().mean().sqrt().item()
-    base = (x).flatten().square().mean().sqrt().item()
+def get_err_ratio(approximation, reference):
+    err = (approximation - reference).flatten().square().mean().sqrt().item()
+    base = (reference).flatten().square().mean().sqrt().item()
     return err / base
 
 
@@ -58,12 +56,12 @@ class Config:
     dtype: torch.dtype = torch.bfloat16
 
 
-def generate_inputs(cfg: Config, apply_layer_norm: bool):
+def generate_inputs(cfg: Config, normalized: bool = False):
     qk_shape = (cfg.batch_size, cfg.seq_len, cfg.num_heads, cfg.dim_qk)
     v_shape = (cfg.batch_size, cfg.seq_len, cfg.num_heads, cfg.dim_v)
     # ln_qk = torch.nn.LayerNorm(cfg.dim_qk, device="cuda", dtype=cfg.dtype) if apply_layer_norm else lambda x: x
     # ln_v = torch.nn.LayerNorm(cfg.dim_v, device="cuda", dtype=cfg.dtype) if apply_layer_norm else lambda x: x
-    norm_f = lambda x: x.normal_()
+    norm_f = lambda x: x.normal_() if normalized else x
     norm_qk = norm_f
     norm_v = norm_f
     head_decays = tuple(get_decays(num_heads=cfg.num_heads, decay_range=cfg.decay_range).cpu().numpy().tolist())
@@ -105,7 +103,7 @@ def benchmark_run_times(cfg: Config):
     # total_flops = 2.0 * cfg.batch_size * cfg.num_heads * cfg.seq_len * cfg.seq_len * (cfg.dim_qk + cfg.dim_v)
     # print("Caveat: TFLOPs might be misleading here, but the larger the faster..")
 
-    inputs = generate_inputs(cfg, apply_layer_norm=False)
+    inputs = generate_inputs(cfg, normalized=False)
 
     latency = do_bench(partial(ref_program, *inputs))
     logger.info("Ref: {:.2f} ms".format(latency))
@@ -369,7 +367,7 @@ def test_backward_pass():
         decay_range=(5, 12),
     )
 
-    Q, K, V, S, head_decays = generate_inputs(cfg, True)
+    Q, K, V, S, head_decays = generate_inputs(cfg, normalized=False)
     Q, K, V, S = Q.requires_grad_(), K.requires_grad_(), V.requires_grad_(), S.requires_grad_()
     dO = torch.randn_like(V)
     dS_new = torch.randn_like(S)

test_pop_retention.py
@@ -6,8 +6,8 @@ from tilelang.profiler import do_bench
 from loguru import logger
 
 from fused_retention.reference import ref_program_
-from pop_retention import fused_pop_retention
-from test_fused_retention import Config as RetNetConfig, generate_inputs, get_err_ratio
+from pop_retention import flash_pop_retention
+from test_fused_retention import Config as RetNetConfig, generate_inputs, get_err_ratio, log_error_info, detail_level
 
 
 def ref_pop(Q, K, V, prev_state, head_decays, block_size: int = 512, *args):
@@ -32,11 +32,11 @@ def ref_pop(Q, K, V, prev_state, head_decays, block_size: int = 512, *args):
     return torch.cat(res, dim=1), torch.stack(states, dim=1)
 
 
-def run_test(cfg, block_size):
+def run_fwd_test(cfg, block_size):
     Q, K, V, S, head_decays = generate_inputs(cfg, False)
 
     O_ref, states_ref = ref_pop(Q, K, V, S, head_decays, block_size=block_size)
-    O, states = fused_pop_retention(Q, K, V, S, head_decays, block_size)
+    O, states = flash_pop_retention(Q, K, V, S, head_decays, block_size)
 
     o_diff = (O_ref - O).abs()
     err_ratio = get_err_ratio(O, O_ref)
@@ -52,10 +52,52 @@ def run_test(cfg, block_size):
     print(f"{states_ref[0, 0, 0]}")
 
     from fused_retention import fused_chunk_retention
-    latency = do_bench(partial(fused_pop_retention, Q, K, V, S, head_decays, block_size))
+    latency = do_bench(partial(flash_pop_retention, Q, K, V, S, head_decays, block_size))
     logger.info("POP: {:.2f} ms".format(latency))
     latency = do_bench(partial(fused_chunk_retention, Q, K, V, S, head_decays))
     logger.info("RetNet: {:.2f} ms".format(latency))
+    latency = do_bench(partial(ref_pop, Q, K, V, S, head_decays, block_size))
+    logger.info("Pytorch Naive POP: {:.2f} ms".format(latency))
+
+
+def run_bwd_test(cfg, block_size):
+    Q, K, V, S, head_decays = generate_inputs(cfg, False)
+    Q, K, V, S = Q.requires_grad_(), K.requires_grad_(), V.requires_grad_(), S.requires_grad_()
+
+    Q32, K32, V32, S32 = Q.float(), K.float(),V.float(),S.float()
+    O_ref, states_ref = ref_pop(Q32, K32, V32, S32, head_decays, block_size=block_size)
+    O, states = flash_pop_retention(Q, K, V, S, head_decays, block_size)
+
+    d_out = torch.randn_like(O)
+    d_states = torch.randn_like(states)
+
+    ((O * d_out.clone()).sum() + (states * d_states.clone()).sum()).backward(retain_graph=True)
+    dQ, Q.grad = Q.grad.clone(), None
+    dK, K.grad = K.grad.clone(), None
+    dV, V.grad = V.grad.clone(), None
+    dS, S.grad = S.grad.clone(), None
+
+    ((O_ref * d_out.clone().float()).sum() + (states_ref * d_states.clone().float()).sum()).backward(retain_graph=True)
+    dQ_ref, Q.grad = Q.grad.clone(), None
+    dK_ref, K.grad = K.grad.clone(), None
+    dV_ref, V.grad = V.grad.clone(), None
+    dS_ref, S.grad = S.grad.clone(), None
+
+    logger.log(detail_level, f"dQ_ref: {dQ_ref.flatten()[:10]}")
+    logger.log(detail_level, f"dQ: {dQ.flatten()[:10]}")
+    log_error_info(dQ, dQ_ref, 'dQ')
+
+    logger.log(detail_level, f"dK_ref: {dK_ref.flatten()[:10]}")
+    logger.log(detail_level, f"dK: {dK.flatten()[:10]}")
+    log_error_info(dK, dK_ref, 'dK')
+
+    logger.log(detail_level, f"dV_ref: {dV_ref.flatten()[:10]}")
+    logger.log(detail_level, f"dV: {dV.flatten()[:10]}")
+    log_error_info(dV, dV_ref, 'dV')
+
+    logger.log(detail_level, f"dS_ref: {dS_ref.flatten()[:10]}")
+    logger.log(detail_level, f"dS: {dS.flatten()[:10]}")
+    log_error_info(dS, dS_ref, 'dS')
 
 
 def test_generation_scenario():
@@ -78,7 +120,7 @@ def test_generation_scenario():
     )
     block_size = 144
 
-    run_test(cfg, block_size)
+    run_fwd_test(cfg, block_size)
 
     cfg = RetNetConfig(
         batch_size=128,
@@ -93,7 +135,7 @@ def test_generation_scenario():
     )
     block_size = 15
 
-    run_test(cfg, block_size)
+    run_fwd_test(cfg, block_size)
 
 
 def test_training_scenario():
@@ -115,7 +157,7 @@ def test_training_scenario():
         block_T=64,
         decay_range=(5, 12),
     )
-    run_test(cfg, block_size)
+    run_fwd_test(cfg, block_size)
 
     block_size = 32
     cfg = RetNetConfig(
@@ -129,7 +171,7 @@ def test_training_scenario():
         block_T=64,
         decay_range=(5, 12),
     )
-    run_test(cfg, block_size)
+    run_fwd_test(cfg, block_size)
 
     block_size = 15
     cfg = RetNetConfig(
@@ -143,9 +185,26 @@ def test_training_scenario():
         block_T=64,
         decay_range=(5, 12),
     )
-    run_test(cfg, block_size)
+    run_fwd_test(cfg, block_size)
+
+
+def test_bwd():
+    block_size = 32
+    cfg = RetNetConfig(
+        batch_size=8,
+        num_heads=4,
+        seq_len=2**15,
+        dim_qk=64,
+        dim_v=64,
+        block_K=64,
+        block_V=64,
+        block_T=64,
+        decay_range=(5, 12),
+    )
+    run_bwd_test(cfg, block_size)
 
 
 if __name__ == '__main__':
-    test_generation_scenario()
-    test_training_scenario()
+    # test_generation_scenario()
+    # test_training_scenario()
+    test_bwd()