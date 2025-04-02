78af21cdadc2144989248ed072ae11b0b831f5f1
leor-c <liorcohen5@gmail.com>
2025-04-02 12:18:13 +0300

    updated init

--- flash_pop/__init__.py
+++ flash_pop/__init__.py
@@ -1,3 +1,5 @@
 from fused_retention import fused_chunk_retention
 from pop_retention import flash_pop_retention
-from retention_layer import MultiScaleRetention, RetNetDecoderLayer
+
+from retnet import RetNetDecoderLayer, RetNetDecoder
+from pop_retnet import POPRetNetDecoder, POPDecoderLayer
